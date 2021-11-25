ff28c516219151bd44f417f63c109136c7505d08
Liat Guvenc <liatg812@gmail.com>
2021-11-25 17:50:53 -0500

    got the map to render

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
@@ -9,8 +8,9 @@ import NavigationTheme from './components/NavigationTheme';
 import BottomNavigator from './components/BottomNavigator';
 import ListingsScreen from './screens/ListingsScreen';
 import LaunchScreen from './screens/LaunchScreen';
-import MyMap from './MyMap.js'
 import SignUpScreen from "./screens/SignUpScreen";
+import React from 'react';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -18,8 +18,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator >
-        <Stack.Screen options={{ headerShown: false }} name="Launch" component={LaunchScreen}/>
-        <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
+        {/* <Stack.Screen options={{ headerShown: false }} name="Launch" component={LaunchScreen}/>
+        <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} /> */}
         <Stack.Screen options={{ headerShown: false }} name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
