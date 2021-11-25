a444d3b476cc6d01117fad4605dad7124ad8699b
Sen <83885190+sencaichi@users.noreply.github.com>
2021-11-25 18:14:23 -0500

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -53,20 +53,6 @@ const HomeScreen = () => {
       </View>
     );
   }
-
-  return (
-    <View style={styles.container}>
-       {/* <MapContainer /> */}
-      <Text>Hi, welcome!</Text>
-      <GoogleMapApp />
-      <TouchableOpacity
-        onPress={handleSignOut}
-        style={styles.button}
-      >
-        <Text style={styles.buttonText}>Set My Current Location</Text>
-      </TouchableOpacity>
-    </View>
-  )
 }
 
 export default HomeScreen
