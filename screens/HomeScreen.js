c28d5d83605d45235b77d50fdf0f092a0cd838b0
Zoe Zhang <zhangyizhuozoe@gmail.com>
2021-11-30 00:23:52 -0500

    added: search bar in home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Text, Dimensions, StyleSheet, SafeAreaView, Image } from 'react-native';
+import { View, Text, Dimensions, StyleSheet, SafeAreaView, Image, ScrollView} from 'react-native';
 import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
+import { SliderBox } from 'react-native-image-slider-box';
+import { SearchBar } from 'react-native-elements';
 
 const { width, height } = Dimensions.get('window');
 const ASPECT_RATIO = width / height;
@@ -46,29 +48,37 @@ export default class HomeScreen extends React.Component {
         longitude: LONGITUDE,
         latitudeDelta: LATITUDE_DELTA,
         longitudeDelta: LONGITUDE_DELTA,
-      }
+      },
+      images: [
+        'https://thecrossingsofdawsonville.com/wp-content/uploads/sites/14/2019/07/Welcome-to-the-team-1200x565.jpg',
+        'https://www.creativefabrica.com/wp-content/uploads/2020/09/23/WELCOME-Graphics-5632158-1.jpg',
+        'https://media.istockphoto.com/photos/on-colourful-speech-bubbles-picture-id180819641?b=1&k=20&m=180819641&s=170667a&w=0&h=CX51cRVofQl95e_cu9Bfy5PLZQ1WdsqmJ-NCFzU96UI=',
+        'https://mobilemonkey.com/wp-content/uploads/2020/12/welcome-greeting-message.png',
+        'https://img.pixers.pics/pho_wat(s3:700/FO/53/86/52/14/700_FO53865214_8d9a5b68feceb7b851e95b5a8f6fd218.jpg,700,525,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,480,475,jpg)/stickers-welcome-tag-cloud-customer-service-greetings-home-smile-card.jpg.jpg'
+      ],
+      search: '',
     };
   }
 
+  updateSearch = (search) => {
+    this.setState({ search });
+  };
+
   render() {
+    const { search } = this.state;
     return (
       // <View style={styles.container}>
         <SafeAreaView>
-          <Text style={styles.text}>Hi, welcome!
-          </Text>
-          {/* <Text>Email: {auth.currentUser?.email}</Text> */}
-            <SafeAreaView style={{
-              backgroundColor: "#E4EFE7",
-              width: '100%',
-              height: 80,
-              borderRadius: 10,
-              justifyContent: 'center',
-              alignItems: 'center',
-              position: 'center'
-            }}>
-              
-            </SafeAreaView>
-
+          <SearchBar
+            placeholder="Type Here..."
+            onChangeText={this.updateSearch}
+            value={search}
+            showCancel
+            lightTheme
+          />
+          <View style={styles.banner}>
+            <SliderBox images={this.state.images} />
+          </View>
         <MapView
           provider={PROVIDER_GOOGLE}
           style={styles.map}
@@ -136,16 +146,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  banner:{
+    marginVertical: 10,
+  },
   text:{
     color: 'black',
     fontWeight: 'bold',
     fontSize: 16,
     textAlign: 'center',
     margin: 15
-  },
-  img:{
-    width: '100%',
-    height: '30%'
   },
   map: {
     height: 500,
