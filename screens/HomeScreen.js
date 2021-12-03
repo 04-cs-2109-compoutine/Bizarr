bf0cd795e3a37c49a3cd3fbc3feb67dd533b48da
Zoe Zhang <zhangyizhuozoe@gmail.com>
2021-12-03 13:29:48 -0500

    filtered out user own listings on all listing page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,7 +5,6 @@ import { View, Text, Dimensions, StyleSheet, SafeAreaView, Image, ScrollView} fr
 import { SliderBox } from "react-native-image-slider-box";
 import { SearchBar } from "react-native-elements";
 import * as Location from 'expo-location'
-import useLocation from '../components/Config/useLocation'
 
 // setting up for a default region and map view size
 const { width, height } = Dimensions.get("window");
@@ -29,8 +28,8 @@ export default class HomeScreen extends React.Component {
         longitudeDelta: LONGITUDE_DELTA,
       },
       images: [
-        require("../assets/uglywelcomebanner.png"),
-        require("../assets/banner2.png"),
+        // require("../assets/uglywelcomebanner.png"),
+        // require("../assets/banner2.png"),
         "https://thecrossingsofdawsonville.com/wp-content/uploads/sites/14/2019/07/Welcome-to-the-team-1200x565.jpg",
         "https://www.creativefabrica.com/wp-content/uploads/2020/09/23/WELCOME-Graphics-5632158-1.jpg",
         "https://media.istockphoto.com/photos/on-colourful-speech-bubbles-picture-id180819641?b=1&k=20&m=180819641&s=170667a&w=0&h=CX51cRVofQl95e_cu9Bfy5PLZQ1WdsqmJ-NCFzU96UI=",
