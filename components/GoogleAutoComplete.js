01f15cef17c076ae3ef5226c3c13986be8e0e026
Grace Lin <gracelin3295@gmail.com>
2021-12-05 01:26:30 -0500

    connected google autocomplete to map and added pins with descriptions

--- components/GoogleAutoComplete.js
+++ components/GoogleAutoComplete.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Text, SafeAreaView, Image } from "react-native";
+import Axios from "axios";
+import { View, StyleSheet } from "react-native";
 import Constants from "expo-constants";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
-import MapView, { Marker, Callout, PROVIDER_GOOGLE } from "react-native-maps";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 
 if (process.env.NODE_ENV !== "production") require("../secrets");
 const GOOGLE_PLACES_API_KEY = process.env.GOOGLE_PLACES_API_KEY;
@@ -10,8 +11,9 @@ const GOOGLE_PLACES_API_KEY = process.env.GOOGLE_PLACES_API_KEY;
 //auto complete with geo coding info
 const App = () => {
   const [location, setLocation] = useState({
-    latitude: 40.7675,
-    longitude: 73.8331,
+    latitude: 40.752714,
+    longitude: -73.97722689999999,
+    description: "Grand central station, East 42nd Street, New York, NY, USA",
   });
   const [pin, setPin] = useState({});
 
@@ -23,25 +25,55 @@ const App = () => {
           key: GOOGLE_PLACES_API_KEY,
           language: "en", // language of the results
         }}
-        onPress={(data, details = null) => console.log(data)}
+        onPress={async (data, details = null) => {
+          const { data: res } = await Axios.get(
+            `https://maps.googleapis.com/maps/api/place/details/json?input=bar&placeid=${data.place_id}&key=${GOOGLE_PLACES_API_KEY}`
+          );
+          setLocation({
+            latitude: res.result.geometry.location.lat,
+            longitude: res.result.geometry.location.lng,
+            description: data.description,
+          });
+        }}
         onFail={(error) => console.error(error)}
         requestUrl={{
           url:
             "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api",
           useOnPlatform: "web",
-        }} // this in only required for use on the web. See https://git.io/JflFv more for details.
+        }}
       />
       <MapView
         provider={PROVIDER_GOOGLE}
         style={{ flex: 1 }}
         region={{
-          latitude: 40.768452,
-          longitude: -73.832764,
-          latitudeDelta: 0.056,
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: 0.026,
           longitudeDelta: 0.006866,
         }}
         showsUserLocation
-      />
+      >
+        <Marker
+          draggable={true}
+          coordinate={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+          }}
+          centerOffset={{ x: -42, y: -60 }}
+          anchor={{ x: 0.84, y: 1 }}
+          title={location.description}
+          onDragEnd={(e) => {
+            setPin({
+              latitude: e.nativeEvent.coordinate.latitude,
+              longitude: e.nativeEvent.coordinate.longitude,
+            });
+            setLocation({
+              latitude: e.nativeEvent.coordinate.latitude,
+              longitude: e.nativeEvent.coordinate.longitude,
+            });
+          }}
+        ></Marker>
+      </MapView>
     </View>
   );
 };
@@ -56,77 +88,3 @@ const styles = StyleSheet.create({
 });
 
 export default App;
-
-//---------------------------------AUTOCOMPLETE WITH FORM CODE(not react)-----------------
-//autocomplete
-// ("use strict");
-
-// function initMap() {
-//   const componentForm = [
-//     "location",
-//     "locality",
-//     "administrative_area_level_1",
-//     "country",
-//     "postal_code",
-//   ];
-//   const map = new google.maps.Map(document.getElementById("map"), {
-//     zoom: 11,
-//     center: { lat: 37.4221, lng: -122.0841 },
-//     mapTypeControl: false,
-//     fullscreenControl: true,
-//     zoomControl: true,
-//     streetViewControl: true,
-//   });
-//   const marker = new google.maps.Marker({ map: map, draggable: false });
-//   const autocompleteInput = document.getElementById("location");
-//   const autocomplete = new google.maps.places.Autocomplete(autocompleteInput, {
-//     fields: ["address_components", "geometry", "name"],
-//     types: ["address"],
-//   });
-//   autocomplete.addListener("place_changed", function () {
-//     marker.setVisible(false);
-//     const place = autocomplete.getPlace();
-//     if (!place.geometry) {
-//       // User entered the name of a Place that was not suggested and
-//       // pressed the Enter key, or the Place Details request failed.
-//       window.alert("No details available for input: '" + place.name + "'");
-//       return;
-//     }
-//     renderAddress(place);
-//     fillInAddress(place);
-//   });
-
-//   function fillInAddress(place) {
-//     // optional parameter
-//     const addressNameFormat = {
-//       street_number: "short_name",
-//       route: "long_name",
-//       locality: "long_name",
-//       administrative_area_level_1: "short_name",
-//       country: "long_name",
-//       postal_code: "short_name",
-//     };
-//     const getAddressComp = function (type) {
-//       for (const component of place.address_components) {
-//         if (component.types[0] === type) {
-//           return component[addressNameFormat[type]];
-//         }
-//       }
-//       return "";
-//     };
-//     document.getElementById("location").value =
-//       getAddressComp("street_number") + " " + getAddressComp("route");
-//     for (const component of componentForm) {
-//       // Location field is handled separately above as it has different logic.
-//       if (component !== "location") {
-//         document.getElementById(component).value = getAddressComp(component);
-//       }
-//     }
-//   }
-
-//   function renderAddress(place) {
-//     map.setCenter(place.geometry.location);
-//     marker.setPosition(place.geometry.location);
-//     marker.setVisible(true);
-//   }
-// }
