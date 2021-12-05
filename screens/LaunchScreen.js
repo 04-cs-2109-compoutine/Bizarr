de2e702d91805b6df46cab261e36c2bf83cc523c
Liat Guvenc <liatg812@gmail.com>
2021-12-05 16:41:04 -0500

    uploaded gif

--- screens/LaunchScreen.js
+++ screens/LaunchScreen.js
@@ -1,18 +1,41 @@
-import React from "react";
-import { StyleSheet, Text, View, Image, Animated } from "react-native";
+
+import { StyleSheet, Text, View, Image, SafeAreaView} from "react-native";
 import { Button } from "react-native-elements";
 import RaisedButton from "../components/Button/launchButton";
 import Login from "./LoginScreen";
 import Screen from "../components/Screen";
+import React, { useState, useEffect } from 'react';
+const mapGif = require('../assets/image/transpmap2.gif')
+import FastImage from 'react-native-fast-image'
+
 
 const LaunchScreen = ({ navigation }) => {
+  [isReady, setIsReady] = useState(false)
+  useEffect( () => {
+    console.log(isReady);
+}, [isReady]);
+
   return (
-    <Screen>
       <View style={styles.container}>
-        <Image
+        {/* <Image
           style={styles.logo}
           source={require("../assets/image/logotransparent.png")}
-        />
+        /> */}
+         <Image
+          source={mapGif}
+          // resizeMode="cover"
+          style={styles.image}
+        /> 
+        {/* <FastImage
+    style={styles.image}
+    source={{
+      uri: mapGif,
+      headers: { Authorization: 'token' },
+      priority: FastImage.priority.normal,
+      cache: FastImage.cacheControl.immutable,
+    }}
+    resizeMode={FastImage.resizeMode.contain}
+/> */}
         <View style={styles.buttonsContainer}>
           <RaisedButton
             text="Login"
@@ -24,7 +47,6 @@ const LaunchScreen = ({ navigation }) => {
           />
         </View>
       </View>
-    </Screen>
   );
 };
 
@@ -33,11 +55,18 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "#5C8D89"
+  },
+  imageContainer: {
+    flex: 1,
+    left: 20,
   },
   image: {
     flex: 1,
-    justifyContent: "center",
-    resizeMode: "stretch",
+    // justifyContent: "center",
+    // resizeMode: "stretch",
+    width: "100%",
+    height: "100%"
   },
   logo: {
     width: 300,
