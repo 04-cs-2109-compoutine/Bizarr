ef657c3832b23c0eda76cb53a047f5f0c32be77e
Zoe Zhang <zhangyizhuozoe@gmail.com>
2021-12-06 12:58:24 -0500

    account detail update user

--- screens/AccountDetailsScreen.js
+++ screens/AccountDetailsScreen.js
@@ -47,7 +47,7 @@ function AccountDetailsScreen() {
           displayName: name,
           phone: phone,
           location: location,
-          email: email,
+          photoURL: photoURL
         });
       });
   };
