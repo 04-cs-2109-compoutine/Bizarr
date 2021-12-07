8950a2f439b4ac6f50f0495b0228d4a241a4f6b1
Liat Guvenc <liatg812@gmail.com>
2021-12-07 13:55:13 -0500

    took out the code for th splash screen

--- App.js
+++ App.js
@@ -7,40 +7,34 @@ import AppLoading from "expo-app-loading";
 import { Asset } from "expo-asset";
 
 export default function App() {
-  const [launchLoaded, setLaunchLoaded] = useState(false);
-  const _cacheResourcesAsync = async () => {
-    try{
-    const images = [
-      require("./assets/image/transpmap2.gif"),
-      require("./assets/B.png"),
-    ];
+  // const [launchLoaded, setLaunchLoaded] = useState(false);
+  // const _cacheResourcesAsync = async () => {
+  //   try{
+  //   const images = [
+  //     require("./assets/image/transpmap2.gif"),
+  //     require("./assets/B.png"),
+  //   ];
   
-    const cacheImages = images.map((image) => {
-      if (typeof image === 'string') {
-        return Image.prefetch(image);
-      } else {
-      return Asset.fromModule(image).downloadAsync();
-    }
-  });
-  return Promise.all(cacheImages);
-  }catch(e){
-    console.log(e)
+  //   const cacheImages = images.map((image) => {
+  //     if (typeof image === 'string') {
+  //       return Image.prefetch(image);
+  //     } else {
+  //     return Asset.fromModule(image).downloadAsync();
+  //   }
+  // });
+  // return Promise.all(cacheImages);
+  // }catch(e){
+  //   console.log(e)
 
-  }
-  };
+  // }
+  // };
   
-  return ( launchLoaded === true ? (
+  return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <Main />
       </PersistGate>
     </Provider>
-  ) : (
-    <AppLoading
-      startAsync={() =>_cacheResourcesAsync}
-      onFinish={() => setLaunchLoaded(true)}
-      onError={console.warn}
-    />
-  )
+
   );
 }
